6ae61631ec721961969d09c0e18ea5c99c4795e5
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-06-30 16:25:11 +0300

    updating frontent components

---
+++ frontend_react/src/components/index.js
@@ -0,0 +1,2 @@
+// Desc: Index file for all components
+export {default as Navbar} from './Navbar/Navbar';
---
+++ frontend_react/src/container/index.js
@@ -0,0 +1,9 @@
+// Desc: Index file for container, instead of importing each component in App.js, we can import all components from container/index.js
+//Exclude component Navbar because it's simple !!!
+
+export {default as About} from './About/About.jsx';
+export {default as Footer} from './Footer/Footer.jsx';
+export {default as Header} from './Header/Header.jsx';
+export {default as Skills} from './Skills/Skills.jsx';
+export {default as Work} from './Work/Work.jsx';
+export {default as Testimonial} from './Testimonial/Testimonial.jsx';
