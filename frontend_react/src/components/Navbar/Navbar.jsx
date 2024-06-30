706f8e5e8a6b4fab2db59006ee4e9e4417f3db29
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-06-30 16:39:15 +0300

    updating images and navbar compo

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './Navbar.scss';
 
+import {images} from '../../constants'; // Importing images from constants folder
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        <div>
+            <img src={images.logo} alt='logo'/>
+        </div>
+        <ul>
+            {['home', 'about', 'contact', 'work', 'skills', 'contact'].map((item) => (
+                <li key={`link-${item}`}>
+                    <div/>
+                    <a href={`#${item}`}>{item}</a>
+                </li>
+            ))}
+        </ul>
+    </nav>
   );
 };
 
---
+++ frontend_react/src/constants/index.js
@@ -0,0 +1,2 @@
+// Desc: Constants for the project images, we can import all images from constants/index.js
+export {default as images} from './images';
