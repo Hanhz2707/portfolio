1b61dc037577e53991cf54314c489cd4c5f3ec8b
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-07-03 15:34:24 +0300

    using HOC for small components and applying icons for social media

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react';
 import './About.scss';
 
 import {motion} from 'framer-motion';
-import {images} from '../../constants';
 import { urlFor, client } from '../../client';
+import { AppWrap } from '../../wrapper';
 
 // const abouts = [
 //   {
@@ -71,4 +71,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
