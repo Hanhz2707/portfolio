e9b532652975c3d9bf5c593bc9638f56100fb351
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-07-07 14:59:50 +0300

    Adding brands to testimonial section

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -16,6 +16,10 @@ const Testimonial = () => {
   
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
   useEffect(() => {
     // mispelling testmonials instead of testimonials
     const query = '*[_type == "testmonials"]';
@@ -32,16 +36,63 @@ const Testimonial = () => {
       })
   }, []);
 
+  //Shorthand for testimonial (testimonials[currentIndex])
+  const test = testimonials[currentIndex];
+
   return (
     <>
     {/* check if testimonial exitst */}
       {testimonials.length && (
         <>
           <div className='app__testimonial-item app__flex'>
-            <img src={urlFor(testimonials[currentIndex].imageurl)} alt="testimonial" />
+            <img src={urlFor(test.imageurl)} alt="testimonial" />
+
+            <div className='app__testimonial-content'>
+              <p className='p-text'>{test.feedback}</p>
+              <div>
+                <h4 className='bold-text'>{test.name}</h4>
+                <h5 className='p-text'>{test.company}</h5>
+              </div>
+            </div>
           </div>
+
+          <div className='app__testimonial-btns app__flex'>
+
+            {/* if currentIndex is the 1st one, we will move to the last testimonial , if not, we will move to the previous one */}
+
+            <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+              
+              {/* if currentIndex is the last one, we will move to the 1st testimonial , if not, we will move to the next one */}
+            <div className='app__flex' onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+
+
+
+          </div>
+
+
+          
         </>
       )}
+
+      <div className='app__testimonials-brands app__flex'>
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{opacity: [0, 1]}}
+            transition={{duration: 0.5, type: 'tween'}}
+            key={brand._id}
+
+          >
+            <img src={urlFor(brand.imageurl)} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
+
+
+
     </>
   );
 };
