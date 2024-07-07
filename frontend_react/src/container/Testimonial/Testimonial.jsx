5e619fc82c5b0617d56de4fe71414ff4334287e0
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-07-07 14:24:22 +0300

    Syntax error || fix bug for testimonials section (mispelling)

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -13,10 +13,42 @@ const Testimonial = () => {
 
   const [brands, setBrands] = useState([]);
   const [testimonials, setTestimonials] = useState([]);
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    // mispelling testmonials instead of testimonials
+    const query = '*[_type == "testmonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setTestimonials(data);
+      })
+
+    client.fetch(brandsQuery)
+      .then((data) => {
+        setBrands(data);
+      })
+  }, []);
 
   return (
-    <div>Testimonial</div>
+    <>
+    {/* check if testimonial exitst */}
+      {testimonials.length && (
+        <>
+          <div className='app__testimonial-item app__flex'>
+            <img src={urlFor(testimonials[currentIndex].imageurl)} alt="testimonial" />
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
-export default Testimonial;
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg'
+
+);
