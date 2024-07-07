3d1f52e3dde0f3966ea015ad4567227319999b88
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-07-07 15:46:07 +0300

    Adding Email and TelNumber for Contact section

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -8,7 +8,23 @@ import {client} from '../../client';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    
+    <>
+      <h2 className='head-text'>Take a coffee and chat with me</h2>
+
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt="email" />
+          <a href="mailto:phamhuyanh277@gmail.com" className='p-text'>phamhuyanh277@gmail.com</a>
+        </div>
+
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel: +358468953901" className='p-text'>+358468953901</a>
+        </div>
+
+      </div>
+    </>
   );
 };
 
