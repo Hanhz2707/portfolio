bdd65fe024faebfe8a7fe61604f0b2b5e2aa1995
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-07-07 13:57:50 +0300

    starting with testimonial section

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Testimonial.scss';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+
+
+
 
 const Testimonial = () => {
+
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+
   return (
     <div>Testimonial</div>
   );
