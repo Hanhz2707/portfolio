93b1f28a606683abe4b074ebec1774f3ce93cc42
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-07-08 14:39:49 +0300

    Updating social media icon

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import {BsTwitter, BsFacebook, BsInstagram, BsLinkedin} from 'react-icons/bs';
+import {BsTwitter, BsFacebook, BsInstagram, BsLinkedin, BsGithub} from 'react-icons/bs';
 import {FaFacebookF, FaTwitter, FaInstagram, FaLinkedin} from 'react-icons/fa';
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-        <div>
-            <BsTwitter />
+              <div>
+            <a href='https://www.facebook.com/profile.php?id=100008323577440' target='_blank'>
+              <div>
+                <BsFacebook />
+              </div>
+            </a>
         </div>
+
         <div>
-            <FaFacebookF />
+            <a href='https://www.linkedin.com/in/huy-anh-pham-890b63226/' target='_blank'>
+              <div>
+                <BsLinkedin />
+              </div>
+            </a>
         </div>
+
         <div>
-            <BsInstagram />
+            <a href='https://github.com/Hanhz2707' target='_blank'>
+              <div>
+                <BsGithub />
+              </div>
+            </a>
         </div>
     </div>
   );
